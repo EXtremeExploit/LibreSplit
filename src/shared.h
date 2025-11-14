225490a1971fd93c30e28e7d23de4b761552a9e8
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2025-11-14 16:53:29 -0300

    Add ctl

---
+++ src/shared.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+void getXDGruntimeDir(char* buffer, size_t size);
+
+typedef enum : uint8_t {
+    CTL_CMD_START_SPLIT,
+    CTL_CMD_STOP_RESET,
+    CTL_CMD_CANCEL,
+    CTL_CMD_UNSPLIT,
+    CTL_CMD_SKIP,
+    CTL_CMD_EXIT,
+} CTLCommand;
+
+typedef struct __attribute__((__packed__)) CTLMessage {
+    uint32_t length; // Size of message in bytes
+    uint8_t message[];
+} CTLMessage;
